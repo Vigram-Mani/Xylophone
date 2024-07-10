4afe9c2cfd80a75ecd0abc7db97059c7526761c1
VIGRAM K M <vigramkm@gmail.com>
2024-07-10 19:06:59 +0530

    Initial Commit

---
+++ lib/main.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+import 'package:audioplayers/audioplayers.dart';
+
+void main() {
+  runApp(const XylophoneApp());
+}
+
+class XylophoneApp extends StatelessWidget {
+  const XylophoneApp({super.key});
+
+  void playSound(int soundNumber) {
+    final player = AudioPlayer();
+    player.play(
+      AssetSource("note$soundNumber.wav"),
+    );
+  }
+
+  Expanded buildKey({required Color color, required int soundNumber}) {
+    return Expanded(
+      child: TextButton(
+        child: Container(
+          color: color,
+          margin: null,
+        ),
+        onPressed: () {
+          playSound(soundNumber);
+        },
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: SafeArea(
+          child: Container(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: <Widget>[
+                buildKey(color: Colors.red, soundNumber: 1),
+                buildKey(color: Colors.blue, soundNumber: 2),
+                buildKey(color: Colors.green, soundNumber: 3),
+                buildKey(color: Colors.orange, soundNumber: 4),
+                buildKey(color: Colors.yellow, soundNumber: 5),
+                buildKey(color: Colors.black, soundNumber: 6),
+                buildKey(color: Colors.purple, soundNumber: 7),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
